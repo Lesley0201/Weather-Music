52395936a488c1d84a87ea808aae70c8b7d18791
Lesley0201 <114908078+Lesley0201@users.noreply.github.com>
2024-06-06 07:44:17 -0700

    Create firebaseConfig.js

---
+++ Leyi-FinalProject-main/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQgsYB4DofFPMZxTiMKp2d_6hiSvFgpCU",
+  authDomain: "weather-music-71f0a.firebaseapp.com",
+  projectId: "weather-music-71f0a",
+  storageBucket: "weather-music-71f0a.appspot.com",
+  messagingSenderId: "543306765214",
+  appId: "1:543306765214:web:f18d3e4aa2600509d93c8f",
+  measurementId: "G-FKBCQEGEGL"
+};
